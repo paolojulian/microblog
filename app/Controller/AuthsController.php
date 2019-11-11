4360d1d077a1fc6c18b7ce9241257b66cef4a47f
unknown <paolovincent.yns@gmail.com>
2019-11-11 15:32:21 +0800

    test

--- app/Controller/AuthsController.php
+++ app/Controller/AuthsController.php
@@ -27,167 +27,167 @@ class AuthsController extends AppController
         die();
     }
 
-    /**
-     * [GET] /users/search/:searchText.json
-     * [PRIVATE] - only for loggedin user
-     * 
-     * @return json - array of users
-     */
-    public function search($searchText)
-    {
-        $this->request->allowMethod('get');
-        $users = $this->User->searchUser(
-            $searchText,
-            $this->request->user->id,
-        );
-        return $this->responseData($users);
-    }
+    // /**
+    //  * [GET] /users/search/:searchText.json
+    //  * [PRIVATE] - only for loggedin user
+    //  * 
+    //  * @return json - array of users
+    //  */
+    // public function search($searchText)
+    // {
+    //     $this->request->allowMethod('get');
+    //     $users = $this->User->searchUser(
+    //         $searchText,
+    //         $this->request->user->id,
+    //     );
+    //     return $this->responseData($users);
+    // }
 
-    /**
-     * [POST]
-     * [PUBLIC]
-     * 
-     * Signs up a user,
-     * Sends an activation email after a successful registration
-     * 
-     * @return json
-     */
-    public function register()
-    {
-        $this->request->allowMethod('post');
-        $this->request->data['activation_key'] = $this->HasherHandler->generateRand();
-        if ( ! $this->User->addUser($this->request->data)) {
-            return $this->responseUnprocessableEntity('', $this->User->validationErrors);
-        }
+    // /**
+    //  * [POST]
+    //  * [PUBLIC]
+    //  * 
+    //  * Signs up a user,
+    //  * Sends an activation email after a successful registration
+    //  * 
+    //  * @return json
+    //  */
+    // public function register()
+    // {
+    //     $this->request->allowMethod('post');
+    //     $this->request->data['activation_key'] = $this->HasherHandler->generateRand();
+    //     if ( ! $this->User->addUser($this->request->data)) {
+    //         return $this->responseUnprocessableEntity('', $this->User->validationErrors);
+    //     }
 
-        try {
-            // TODO add a page if mail failed,
-            // should display resend link
-            $this->UserHandler->sendActivationMail($this->request->data);
-        } catch (Exception $e) {
-            throw new InternalErrorException(__($e->getMessage()));
-        }
+    //     try {
+    //         // TODO add a page if mail failed,
+    //         // should display resend link
+    //         $this->UserHandler->sendActivationMail($this->request->data);
+    //     } catch (Exception $e) {
+    //         throw new InternalErrorException(__($e->getMessage()));
+    //     }
 
-        return $this->responseOK();
-    }
+    //     return $this->responseOK();
+    // }
 
-    /**
-     * [PUT]
-     * [PRIVATE] - logged in users only
-     * 
-     * Edits current user
-     * 
-     * @return json
-     */
-    public function edit()
-    {
-        $this->request->allowMethod('put');
-        try {
-            if ( ! $this->User->editUser($this->request->user->id, $this->request->data)) {
-                return $this->responseUnprocessableEntity('', $this->User->validationErrors);
-            }
-        } catch (Exception $e) {
-            throw new InternalErrorException(__($e->getMessage()));
-        }
+    // /**
+    //  * [PUT]
+    //  * [PRIVATE] - logged in users only
+    //  * 
+    //  * Edits current user
+    //  * 
+    //  * @return json
+    //  */
+    // public function edit()
+    // {
+    //     $this->request->allowMethod('put');
+    //     try {
+    //         if ( ! $this->User->editUser($this->request->user->id, $this->request->data)) {
+    //             return $this->responseUnprocessableEntity('', $this->User->validationErrors);
+    //         }
+    //     } catch (Exception $e) {
+    //         throw new InternalErrorException(__($e->getMessage()));
+    //     }
 
-        return $this->responseOK();
-    }
+    //     return $this->responseOK();
+    // }
 
-    /**
-     * [DELETE]
-     * [PRIVATE] - Logged in user only
-     * 
-     * @return json
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod('delete');
-        $this->User->id = $id;
-        if ( ! $this->User->exists()) {
-            throw new NotFoundException(__('Invalid user'));
-        }
-        if ( ! $this->User->delete()) {
-            throw new InternalErrorException(__('Server Error'));
-        }
+    // /**
+    //  * [DELETE]
+    //  * [PRIVATE] - Logged in user only
+    //  * 
+    //  * @return json
+    //  */
+    // public function delete($id = null)
+    // {
+    //     $this->request->allowMethod('delete');
+    //     $this->User->id = $id;
+    //     if ( ! $this->User->exists()) {
+    //         throw new NotFoundException(__('Invalid user'));
+    //     }
+    //     if ( ! $this->User->delete()) {
+    //         throw new InternalErrorException(__('Server Error'));
+    //     }
 
-        $this->responseDeleted();
-    }
+    //     $this->responseDeleted();
+    // }
 
-    /**
-     * [POST]
-     * [PUBLIC]
-     * 
-     * Logs in the current user and returns a Jwt Token upon success
-     * Only allow activated accounts
-     * 
-     * @return json - containing Jwt Token
-     */
-    public function login()
-    {
-        $this->request->allowMethod('post');
+    // /**
+    //  * [POST]
+    //  * [PUBLIC]
+    //  * 
+    //  * Logs in the current user and returns a Jwt Token upon success
+    //  * Only allow activated accounts
+    //  * 
+    //  * @return json - containing Jwt Token
+    //  */
+    // public function login()
+    // {
+    //     $this->request->allowMethod('post');
 
-        $user = $this->User->authenticate($this->request->data);
-        if ( ! $user) {
-            throw new BadRequestException(__('Invalid Username or Password'));
-        }
+    //     $user = $this->User->authenticate($this->request->data);
+    //     if ( ! $user) {
+    //         throw new BadRequestException(__('Invalid Username or Password'));
+    //     }
 
-        if ($user['User']['is_activated'] != 1) {
-            throw new BadRequestException(__('Please activate your account first.'));
-        }
+    //     if ($user['User']['is_activated'] != 1) {
+    //         throw new BadRequestException(__('Please activate your account first.'));
+    //     }
 
-        return $this->responseData($this->jwtEncode($user["User"]));
-    }
+    //     return $this->responseData($this->jwtEncode($user["User"]));
+    // }
 
-    /**
-     * [GET]
-     * [PRIVATE] - for logged in users only
-     * 
-     * Checks the mutual friends of a certain friend
-     * and currently logged in user
-     * 
-     * @param username - user to be checked
-     * @return json - array of Users
-     */
-    public function mutual($username)
-    {
-        $this->request->allowMethod('get');
-        $friendUser = $this->User->findByUsername($username, 'id');
-        return $this->responseData(
-            $this->User->getMutualFriends(
-                $this->request->user->id,
-                $friendUser['User']['id']
-            )
-        );
-    }
+    // /**
+    //  * [GET]
+    //  * [PRIVATE] - for logged in users only
+    //  * 
+    //  * Checks the mutual friends of a certain friend
+    //  * and currently logged in user
+    //  * 
+    //  * @param username - user to be checked
+    //  * @return json - array of Users
+    //  */
+    // public function mutual($username)
+    // {
+    //     $this->request->allowMethod('get');
+    //     $friendUser = $this->User->findByUsername($username, 'id');
+    //     return $this->responseData(
+    //         $this->User->getMutualFriends(
+    //             $this->request->user->id,
+    //             $friendUser['User']['id']
+    //         )
+    //     );
+    // }
 
-    /**
-     * [GET]
-     * [PRIVATE] - for logged in users only
-     * 
-     * Fetches not yet followed users
-     * Prioritizes friends of friends with mutual connections
-     * and then previously created users
-     * 
-     * TODO
-     * for improvement should add location of user
-     * and prioritize same locations
-     * 
-     * @return json - array of Users
-     */
-    public function notfollowed()
-    {
-        $this->request->allowMethod('get');
-        $page = $this->request->query('page');
-        if ( ! $page) {
-            $page = 1;
-        }
-        return $this->responseData(
-            $this->User->getFriendsOfFriends(
-                $this->request->user->id,
-                $page
-            )
-        );
-    }
+    // /**
+    //  * [GET]
+    //  * [PRIVATE] - for logged in users only
+    //  * 
+    //  * Fetches not yet followed users
+    //  * Prioritizes friends of friends with mutual connections
+    //  * and then previously created users
+    //  * 
+    //  * TODO
+    //  * for improvement should add location of user
+    //  * and prioritize same locations
+    //  * 
+    //  * @return json - array of Users
+    //  */
+    // public function notfollowed()
+    // {
+    //     $this->request->allowMethod('get');
+    //     $page = $this->request->query('page');
+    //     if ( ! $page) {
+    //         $page = 1;
+    //     }
+    //     return $this->responseData(
+    //         $this->User->getFriendsOfFriends(
+    //             $this->request->user->id,
+    //             $page
+    //         )
+    //     );
+    // }
 
     /**
      * [POST]
